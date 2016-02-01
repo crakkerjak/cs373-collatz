045fd5baa42b1bf2ddcdbe2976cdd83a79a4c114
ctimaeus <timaeus@utexas.edu>
2016-02-01 16:05:36 -0600

    Style edits. Final commit, methinks.

--- TileFactory.py
+++ TileFactory.py
@@ -2,23 +2,20 @@
 from array import array
 from Collatz import collatz_eval
 
-tiles = 10000
-upper_bound = 1000000
+TILES = 10000
+UPPER_BOUND = 1000000
 
-tile_size = upper_bound // tiles
-# tile_no = 0
+tile_size = UPPER_BOUND // TILES
 lengths = []
-for i in range (1, upper_bound, tile_size) :
+for i in range (1, UPPER_BOUND, tile_size) :
   j = i + tile_size - 1
-  if j == upper_bound :
+  if j == UPPER_BOUND :
     j -= 1
   max_len = collatz_eval(i, j)
   lengths.append(max_len)
-  # print (str(tile_no), str(i), str(j), str(max_len))
-  # tile_no += 1
 
-print ('tiles = array(\'i\',[' + str(lengths[0]), end='')
+print ('TILES = array(\'i\',[' + str(lengths[0]), end='')
 for l in range(1, len(lengths)) :
   print (',' + str(lengths[l]), end='')
 print ('])')
-# index into array with [n // (upper_bound // tiles)]
+# index into array with [n // (UPPER_BOUND // TILES)]
