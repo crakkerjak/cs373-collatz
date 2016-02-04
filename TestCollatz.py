c5eba07c8390f2e01358564e1e5a36af183e95a7
ctimaeus <timaeus@utexas.edu>
2016-02-03 18:26:03 -0600

    Updated import formatting.

--- TestCollatz.py
+++ TestCollatz.py
@@ -15,7 +15,11 @@
 from io       import StringIO
 from unittest import main, TestCase
 
-from Collatz  import collatz_read, cycle_length, collatz_eval, collatz_print, collatz_solve
+from Collatz  import collatz_read,
+                     cycle_length, 
+                     collatz_eval, 
+                     collatz_print, 
+                     collatz_solve
 
 # -----------
 # TestCollatz
